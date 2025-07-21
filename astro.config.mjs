69c0db3f7d6fcafb9f06b59c4193e2e7a8c4ae74
13101413350 <152783672+13101413350@users.noreply.github.com>
2025-07-21 20:36:02 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
+	site: "https://cf.666731.xyz/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
